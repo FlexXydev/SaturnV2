fd12e674e9c80405a06c187b31fcf68d2894e58a
Bexeos <lolmdrlili94360@gmail.com>
2023-06-11 15:25:58 +0200

    I don't remeber what I done so just explore the files

--- Schemas/admin pannel/botstats.js
+++ Schemas/admin pannel/botstats.js
@@ -2,8 +2,7 @@ const { model, Schema } = require ('mongoose');
 
 let serverSchema = new Schema({
     TotserverCount: { type: String, required: true },
-    totmemberCount: { type: Number, required: true },
-    totcommands: { type: Number, required: true }
+    totmemberCount: { type: Number, required: true }
   });
   
 module.exports = model('serverSchema', serverSchema);
